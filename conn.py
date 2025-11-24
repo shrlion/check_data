9fe1d0bf65ba73a276c0c3ba1567ac167ddac898
fomalhaut <3154242773@qq.com>
2025-11-24 22:47:30 +0800

    add-function

--- conn.py
+++ conn.py
@@ -1,12 +1,66 @@
 import pymysql
 import configparser
+import logging
 
 
-cf = configparser.ConfigParser()
+# 读取配置文件函数
+def config():
 
-cf.read("config.ini")
+    cf = configparser.ConfigParser()
+    cf.read('config.ini')
 
-sections = cf.sections()
 
-for section in sections:
-    print(section)
+
+    sections = cf.sections()
+
+    for section in sections:
+
+        host = cf.get(section, 'host')
+        user = cf.get(section, 'user')
+        passwd = cf.get(section, 'password')
+        db = cf.get(section, 'database')
+
+        db_conn(host, user, passwd, db)
+
+
+        print(section)
+
+    print(sections)
+
+
+# 连接数据库函数
+def db_conn(host,user,passwd,db):
+
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        filename = "./logs/db.conn.log"
+    )
+
+    try:
+        db = pymysql.connect(
+            host = host,
+            user = user,
+            passwd = passwd,
+            database = db
+        )
+    except Exception as e:
+        logging.error(f"This is {e}")
+
+
+    cursor = db.cursor()
+
+    cursor.execute('show database')
+
+
+    db.close()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    config()
